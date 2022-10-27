a1ea7843ed4d32a626f3d1d87ef9845aa330dfb8
Efunnuga Bamidele <bamideleefunnuga9@gmail.com>
2022-10-27 11:55:31 +0100

    Implement the links and its styles

--- src/App.js
+++ src/App.js
@@ -12,7 +12,60 @@ function App() {
          />
         <h4>Bamidele Efunnuga</h4>     
       </header>
-      
+      <div className="links__section">
+      <a
+      className="App-link"
+      id='twitter'
+      href="https://twitter.com/BamideleEfunnu2"
+      target="_blank"
+    >
+      Twitter link
+    </a>
+      <a
+        className="App-link"
+        id='btn__zuri'
+        href="https://training.zuri.team/"
+        target="_blank"
+      >
+        Zuri Team
+      </a>
+      <a
+        className="App-link"
+        id='books'
+        href="http://books.zuri.team"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+      Zuri Books
+      </a>
+      <a
+        className="App-link"
+        id='book__python'
+        href={`https://books.zuri.team/python-for-beginners?ref_id=<yourslackname>`}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Python Books
+      </a>
+      <a
+        className="App-link"
+        id='pitch'
+        href="https://background.zuri.team"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Background Check for Coders
+      </a>
+      <a
+      className="App-link"
+      id='book__design'
+      href="https://books.zuri.team/design-rules"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      Design Books
+    </a>
+      </div>
     </div>
   );
 }
