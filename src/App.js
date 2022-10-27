d1764778e4937d8cfc6fddda86998f9683be8de0
Efunnuga Bamidele <bamideleefunnuga9@gmail.com>
2022-10-27 12:14:41 +0100

    Update README.md and add Social icons

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
+//Styles
 import './App.css';
-import ProfileImage from './assets/profile-img.jpeg'
 
+//images import from assets folder
+import ProfileImage from './assets/profile-img.jpeg'
+import Slack from './assets/icons8_slack_new.svg'
+import Github from './assets/icons8_github.svg'
 function App() {
   return (
     <div className="App">
@@ -13,59 +17,67 @@ function App() {
         <h4>Bamidele Efunnuga</h4>     
       </header>
       <div className="links__section">
-      <a
-      className="App-link"
-      id='twitter'
-      href="https://twitter.com/BamideleEfunnu2"
-      target="_blank"
-    >
-      Twitter link
-    </a>
-      <a
-        className="App-link"
-        id='btn__zuri'
-        href="https://training.zuri.team/"
-        target="_blank"
-      >
-        Zuri Team
-      </a>
-      <a
-        className="App-link"
-        id='books'
-        href="http://books.zuri.team"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-      Zuri Books
-      </a>
-      <a
-        className="App-link"
-        id='book__python'
-        href={`https://books.zuri.team/python-for-beginners?ref_id=<yourslackname>`}
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Python Books
-      </a>
-      <a
-        className="App-link"
-        id='pitch'
-        href="https://background.zuri.team"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Background Check for Coders
-      </a>
-      <a
-      className="App-link"
-      id='book__design'
-      href="https://books.zuri.team/design-rules"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      Design Books
-    </a>
+            <a
+            className="App-link"
+            id='twitter'
+            href="https://twitter.com/BamideleEfunnu2"
+            target="_blank"
+          >
+            Twitter link
+          </a>
+            <a
+              className="App-link"
+              id='btn__zuri'
+              href="https://training.zuri.team/"
+              target="_blank"
+            >
+              Zuri Team
+            </a>
+            <a
+              className="App-link"
+              id='books'
+              href="http://books.zuri.team"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+            Zuri Books
+            </a>
+            <a
+              className="App-link"
+              id='book__python'
+              href={`https://books.zuri.team/python-for-beginners?ref_id=@ebamidele`}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Python Books
+            </a>
+            <a
+              className="App-link"
+              id='pitch'
+              href="https://background.zuri.team"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Background Check for Coders
+            </a>
+            <a
+            className="App-link"
+            id='book__design'
+            href="https://books.zuri.team/design-rules"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Design Books
+          </a>
       </div>
+      <div className='icon__list'>
+        <a href="">
+          <img src={Slack} id='twitter' alt="slack-icon" width='24' height='24' />
+        </a>
+        <a href="https://github.com/efunnuga-bamidele" target="_blank">
+          <img src={Github} id='slack' alt="github-icon" width='24' height='24'/>
+        </a>
+    </div>
     </div>
   );
 }
